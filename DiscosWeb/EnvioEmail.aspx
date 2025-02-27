094b581a2cf4273b7eb4deb0c035d6ad178ecedc
darius <dariussrz@gmail.com>
2025-02-26 22:52:23 -0300

    Email

---
+++ DiscosWeb/EnvioEmail.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="EnvioEmail.aspx.cs" Inherits="DiscosWeb.EnvioEmail" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <asp:textbox runat="server" ID="txtEmail" cssclass="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Asunto</label>
+                <asp:TextBox runat="server" ID="txtAsunto" cssclass="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Mensaje</label>
+                <asp:TextBox TextMode="MultiLine" runat="server" ID="txtMensaje" cssclass="form-control"/>
+            </div>
+            <asp:Button Text="Aceptar" CssClass="btn btn-primary" OnClick="btnAceptar_Click" ID="btnAceptar" runat="server" />
+        </div>
+        <div class="col"></div>
+    </div>
+
+</asp:Content>
---
+++ data/EmailService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace data
+{
+    public partial class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        public EmailService()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("darius83srz@gmail.com", "uups pncj xmvt avch ");
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "smtp.gmail.com";
+        }
+
+        public void armarCorreo(string emailDestino, string asunto, string cuerpo)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("dariussrz@gmail.com");
+            email.To.Add(emailDestino);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            email.Body = cuerpo;
+
+        }
+
+        public void enviarEmail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
